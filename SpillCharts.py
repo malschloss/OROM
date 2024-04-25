1e27b14d2ef46620150c766878ffa12f0f8384e2
malcolm schlossberg <malschloss@gmail.com>
2024-04-25 16:32:57 -0400

    Updated these charts to have a rolling spill window and save the unloaded spills to disk

--- SpillCharts.py
+++ SpillCharts.py
@@ -37,19 +37,84 @@ class SpillCharts(QWidget):
         
         self.reader = None
 
+        self.MAX_SPILLS = 5
+        self.xScatter = []
+        self.yScatter = []
+        self.zScatter = []
+        self.xErr = []
+        self.yErr = []
+        self.zErr = []
+        self.currentFile = 0
+        self.position = 0
+
+        self.filenames = sorted([filename for filename in os.listdir("Reconstructed") if filename.endswith(".npy")])
+        self.fileCount = len(self.filenames)
+        while (self.fileCount > self.currentFile):
+            if (self.currentFile >= self.MAX_SPILLS):
+                self.vtxPlot.removeItem(self.xScatter[self.position])
+                self.vtyPlot.removeItem(self.yScatter[self.position])
+                self.vtzPlot.removeItem(self.zScatter[self.position])
+                self.vtxPlot.removeItem(self.xErr[self.position])
+                self.vtyPlot.removeItem(self.yErr[self.position])
+                self.vtzPlot.removeItem(self.zErr[self.position])
+            self.reader = DataReader([os.path.join("Reconstructed", filename) for filename in self.filenames], "EVENT")
+            self.reader.current_index = self.currentFile
+            self.reader.grab = "EVENT"
+            self.eidData = np.array(self.reader.read_data())
+            self.reader.grab = "XVERTEX"
+            self.vtxData = np.array(self.reader.read_data())
+            self.reader.grab = "YVERTEX"
+            self.vtyData = np.array(self.reader.read_data())
+            self.reader.grab = "ZVERTEX"
+            self.vtzData = np.array(self.reader.read_data())
+            self.reader.grab = "SPILL"
+            self.sidData = np.array(self.reader.read_data()[0])
+            self.vtxMean = np.array([np.mean(self.vtxData)])
+            self.vtyMean = np.array([np.mean(self.vtyData)])
+            self.vtzMean = np.array([np.mean(self.vtzData)])
+            self.vtxSTD = np.std(self.vtxData)
+            self.vtySTD = np.std(self.vtyData)
+            self.vtzSTD = np.std(self.vtzData)
+            if (self.currentFile < self.MAX_SPILLS):
+                self.xScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255)))
+                self.yScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255)))
+                self.zScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255)))
+                self.xErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtxMean,height=self.vtxSTD,pen=pg.mkPen(0,0,255,255),beam=0.5))
+                self.yErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtyMean,height=self.vtySTD,pen=pg.mkPen(255,0,0,255),beam=0.5))
+                self.zErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtzMean,height=self.vtzSTD,pen=pg.mkPen(0,255,0,255),beam=0.5))
+            self.xErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtxMean,height=self.vtxSTD,pen=pg.mkPen(0,0,255,255),beam=0.5)
+            self.xScatter[self.position].setData(self.sidData,self.vtxMean)
+            self.vtxPlot.addItem(self.xScatter[self.position])
+            self.vtxPlot.addItem(self.xErr[self.position])
+            self.yErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtyMean,height=self.vtySTD,pen=pg.mkPen(255,0,0,255),beam=0.5)
+            self.yScatter[self.position].setData(self.sidData,self.vtyMean)
+            self.vtyPlot.addItem(self.yScatter[self.position])
+            self.vtyPlot.addItem(self.yErr[self.position])
+            self.zErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtzMean,height=self.vtzSTD,pen=pg.mkPen(0,255,0,255),beam=0.5)
+            self.zScatter[self.position].setData(self.sidData,self.vtzMean)
+            self.vtzPlot.addItem(self.zScatter[self.position])
+            self.vtzPlot.addItem(self.zErr[self.position])
+            self.spillString = str(self.sidData)
+            np.savez('SpillVertexMeans/' + self.spillString + '.npz',self.sidData,self.vtxMean,self.vtyMean,self.vtzMean,self.vtxSTD,self.vtySTD,self.vtzSTD)
+            self.currentFile += 1
+            self.position = self.currentFile % self.MAX_SPILLS
+            layout=self.layout()
+
     def UpdateChart(self):
-        self.dt=1000
-        if self.runButton.isChecked():
-            self.vtxPlot.clear()
-            self.vtyPlot.clear()
-            self.vtzPlot.clear()
-            currentFile = 0
+        self.dt=500
         while self.runButton.isChecked():
             self.filenames = sorted([filename for filename in os.listdir("Reconstructed") if filename.endswith(".npy")])
             self.fileCount = len(self.filenames)
-            if (self.fileCount > currentFile):
+            if (self.fileCount > self.currentFile):
+                if (self.currentFile >= self.MAX_SPILLS):
+                    self.vtxPlot.removeItem(self.xScatter[self.position])
+                    self.vtyPlot.removeItem(self.yScatter[self.position])
+                    self.vtzPlot.removeItem(self.zScatter[self.position])
+                    self.vtxPlot.removeItem(self.xErr[self.position])
+                    self.vtyPlot.removeItem(self.yErr[self.position])
+                    self.vtzPlot.removeItem(self.zErr[self.position])
                 self.reader = DataReader([os.path.join("Reconstructed", filename) for filename in self.filenames], "EVENT")
-                self.reader.current_index = currentFile
+                self.reader.current_index = self.currentFile
                 self.reader.grab = "EVENT"
                 self.eidData = np.array(self.reader.read_data())
                 self.reader.grab = "XVERTEX"
@@ -66,21 +131,27 @@ class SpillCharts(QWidget):
                 self.vtxSTD = np.std(self.vtxData)
                 self.vtySTD = np.std(self.vtyData)
                 self.vtzSTD = np.std(self.vtzData)
-                self.xScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255))
-                self.yScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255))
-                self.zScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255))
-                self.xErr = pg.ErrorBarItem(x=self.sidData,y=self.vtxMean,height=self.vtxSTD,pen=pg.mkPen(0,0,255,255),beam=0.5)
-                self.xScatter.addPoints(self.sidData,self.vtxMean)
-                self.vtxPlot.addItem(self.xScatter)
-                self.vtxPlot.addItem(self.xErr)
-                self.yErr = pg.ErrorBarItem(x=self.sidData,y=self.vtyMean,height=self.vtySTD,pen=pg.mkPen(255,0,0,255),beam=0.5)
-                self.yScatter.addPoints(self.sidData,self.vtyMean)
-                self.vtyPlot.addItem(self.yScatter)
-                self.vtyPlot.addItem(self.yErr)
-                self.zErr = pg.ErrorBarItem(x=self.sidData,y=self.vtzMean,height=self.vtzSTD,pen=pg.mkPen(0,255,0,255),beam=0.5)
-                self.zScatter.addPoints(self.sidData,self.vtzMean)
-                self.vtzPlot.addItem(self.zScatter)
-                self.vtzPlot.addItem(self.zErr)
-                currentFile += 1
+                if (self.currentFile < self.MAX_SPILLS):
+                    self.xScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255)))
+                    self.yScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255)))
+                    self.zScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255)))
+                    self.xErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtxMean,height=self.vtxSTD,pen=pg.mkPen(0,0,255,255),beam=0.5))
+                    self.yErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtyMean,height=self.vtySTD,pen=pg.mkPen(255,0,0,255),beam=0.5))
+                    self.zErr.append(pg.ErrorBarItem(x=self.sidData,y=self.vtzMean,height=self.vtzSTD,pen=pg.mkPen(0,255,0,255),beam=0.5))
+                self.xErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtxMean,height=self.vtxSTD,pen=pg.mkPen(0,0,255,255),beam=0.5)
+                self.xScatter[self.position].setData(self.sidData,self.vtxMean)
+                self.vtxPlot.addItem(self.xScatter[self.position])
+                self.vtxPlot.addItem(self.xErr[self.position])
+                self.yErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtyMean,height=self.vtySTD,pen=pg.mkPen(255,0,0,255),beam=0.5)
+                self.yScatter[self.position].setData(self.sidData,self.vtyMean)
+                self.vtyPlot.addItem(self.yScatter[self.position])
+                self.vtyPlot.addItem(self.yErr[self.position])
+                self.zErr[self.position] = pg.ErrorBarItem(x=self.sidData,y=self.vtzMean,height=self.vtzSTD,pen=pg.mkPen(0,255,0,255),beam=0.5)
+                self.zScatter[self.position].setData(self.sidData,self.vtzMean)
+                self.vtzPlot.addItem(self.zScatter[self.position])
+                self.vtzPlot.addItem(self.zErr[self.position])
+                np.savez('SpillVertexMeans/' + self.spillString + '.npz',self.sidData,self.vtxMean,self.vtyMean,self.vtzMean,self.vtxSTD,self.vtySTD,self.vtzSTD)
+                self.currentFile += 1
+                self.position = self.currentFile % self.MAX_SPILLS
                 layout=self.layout()
             QtTest.QTest.qWait(self.dt)
--- StripCharts.py
+++ StripCharts.py
@@ -2,7 +2,7 @@
 
 import sys
 import os
-from numpy import *
+import numpy as np
 from pyqtgraph.Qt import QtGui, QtCore
 from random import randrange, uniform
 from PyQt5 import QtTest
@@ -39,19 +39,60 @@ class StripCharts(QWidget):
         
         self.reader = None
 
+        self.MAX_SPILLS = 5
+        self.xScatter = []
+        self.yScatter = []
+        self.zScatter = []
+        self.currentFile = 0
+        self.position = 0
+
+        self.filenames = sorted([filename for filename in os.listdir("Reconstructed") if filename.endswith(".npy")])
+        self.fileCount = len(self.filenames)
+        self.reader = DataReader([os.path.join("Reconstructed", filename) for filename in self.filenames], "EVENT")
+        while (self.currentFile < self.fileCount):
+            if (self.currentFile >= self.MAX_SPILLS):
+                self.vtxPlot.removeItem(self.xScatter[self.position])
+                self.vtyPlot.removeItem(self.yScatter[self.position])
+                self.vtzPlot.removeItem(self.zScatter[self.position])
+            self.reader.current_index = self.currentFile
+            self.reader.grab = "EVENT"
+            self.eidData = self.reader.read_data()
+            self.reader.grab = "XVERTEX"
+            self.vtxData = self.reader.read_data()
+            self.reader.grab = "YVERTEX"
+            self.vtyData = self.reader.read_data()
+            self.reader.grab = "ZVERTEX"
+            self.vtzData = self.reader.read_data()
+            if (self.currentFile < self.MAX_SPILLS):
+                self.xScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255)))
+                self.yScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255)))
+                self.zScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255)))
+            self.xScatter[self.position].setData(self.eidData,self.vtxData)
+            self.vtxPlot.addItem(self.xScatter[self.position])
+            self.yScatter[self.position].setData(self.eidData,self.vtyData)
+            self.vtyPlot.addItem(self.yScatter[self.position])
+            self.zScatter[self.position].setData(self.eidData,self.vtzData)
+            self.vtzPlot.addItem(self.zScatter[self.position])
+            self.reader.grab = "SPILL"
+            self.sidData = self.reader.read_data()[0]
+            self.spillString = str(self.sidData)
+            np.savez('EventVertexData/' + spillString + '.npz',self.eidData,self.vtxData,self.vtyData,self.vtzData)
+            self.currentFile += 1
+            self.position = self.currentFile % self.MAX_SPILLS
+            layout=self.layout()
+
     def UpdateChart(self):
-        self.dt=1000
-        if self.runButton.isChecked():
-            self.vtxPlot.clear()
-            self.vtyPlot.clear()
-            self.vtzPlot.clear()
-            currentFile = 0
+        self.dt=500
         while self.runButton.isChecked():
             self.filenames = sorted([filename for filename in os.listdir("Reconstructed") if filename.endswith(".npy")])
             self.fileCount = len(self.filenames)
-            if (self.fileCount > currentFile):
+            if (self.fileCount > self.currentFile):
+                if (self.currentFile >= self.MAX_SPILLS):
+                    self.vtxPlot.removeItem(self.xScatter[self.position])
+                    self.vtyPlot.removeItem(self.yScatter[self.position])
+                    self.vtzPlot.removeItem(self.zScatter[self.position])
                 self.reader = DataReader([os.path.join("Reconstructed", filename) for filename in self.filenames], "EVENT")
-                self.reader.current_index = currentFile
+                self.reader.current_index = self.currentFile
                 self.reader.grab = "EVENT"
                 self.eidData = self.reader.read_data()
                 self.reader.grab = "XVERTEX"
@@ -60,15 +101,18 @@ class StripCharts(QWidget):
                 self.vtyData = self.reader.read_data()
                 self.reader.grab = "ZVERTEX"
                 self.vtzData = self.reader.read_data()
-                self.xScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255))
-                self.yScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255))
-                self.zScatter=pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255))
-                self.xScatter.addPoints(self.eidData,self.vtxData)
-                self.vtxPlot.addItem(self.xScatter)
-                self.yScatter.addPoints(self.eidData,self.vtyData)
-                self.vtyPlot.addItem(self.yScatter)
-                self.zScatter.addPoints(self.eidData,self.vtzData)
-                self.vtzPlot.addItem(self.zScatter)
-                currentFile += 1
+                if (self.currentFile < self.MAX_SPILLS):
+                    self.xScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,0,255,255)))
+                    self.yScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(255,0,0,255)))
+                    self.zScatter.append(pg.ScatterPlotItem(size=10,brush=pg.mkBrush(0,255,0,255)))
+                self.xScatter[self.position].setData(self.eidData,self.vtxData)
+                self.vtxPlot.addItem(self.xScatter[self.position])
+                self.yScatter[self.position].setData(self.eidData,self.vtyData)
+                self.vtyPlot.addItem(self.yScatter[self.position])
+                self.zScatter[self.position].setData(self.eidData,self.vtzData)
+                self.vtzPlot.addItem(self.zScatter[self.position])
+                np.savez('EventVertexData/' + spillString + '.npz',self.eidData,self.vtxData,self.vtyData,self.vtzData)
+                self.currentFile += 1
+                self.position = self.currentFile % self.MAX_SPILLS
                 layout=self.layout()
             QtTest.QTest.qWait(self.dt)
